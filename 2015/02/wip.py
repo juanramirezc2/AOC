b531243cf226f37d256dd9a539827e0568ab35ed
Juan Ramirez <jgramirezc2@gmail.com>
2022-12-14 10:20:58 -0500

    2015 02

--- 2015/02/wip.py
+++ 2015/02/wip.py
@@ -2,19 +2,19 @@ input = open('input.txt', 'r').read().strip()
 inputList = input.splitlines()
 
 
-totalArea = 0
+feetsOfRibbon = 0
 
 for x in inputList:
     sideList = x.split("x")
     length = int(sideList[0])
     width = int(sideList[1])
     height = int(sideList[2])
-    totalGiftArea = 2*length*width + 2*width*height + 2*height*length
     sortedSides = sorted([length,width,height])
-    totalArea += (totalGiftArea + sortedSides[0] * sortedSides[1])
+    ribbonForBow = height*length*width 
+    feetsOfRibbon += (ribbonForBow + (sortedSides[0] + sortedSides[0] + sortedSides[1] + sortedSides[1]))
 
 
 
-print(totalArea)
+print(feetsOfRibbon)
 
-print("Result: {}".format(totalArea))
+print("Result: {}".format(feetsOfRibbon))
